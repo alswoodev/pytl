c391aa4366c0db74345acfe16ee3c9464685e856
alswoodev <alswoo.dev@gmail.com>
2026-08-04 03:41:42 +0000

    example: how to use this framework

---
+++ example.py
@@ -0,0 +1,56 @@
+from pytl import Source, Processor, Loader, Pipeline
+
+class ExampleSource(Source[str]):
+    async def provide(self):
+        for item in ["a", "b", "c", "d", "e", "f"]:
+            yield item
+
+class ExampleStreamingSource(Source[str]):
+    async def provide(self):
+        import asyncio
+        while True:
+            yield "abc"
+            await asyncio.sleep(1)
+
+class ExampleProcessor(Processor[str, str]):
+    def process(self, txt: str):
+        return txt.upper()
+
+class ExampleLoader(Loader[str]):
+    async def load(self, chunk: list[str]):
+        print(chunk)
+
+batch_pipeline = (
+    Pipeline
+    .builder()
+    .source(ExampleSource(chunk_size=2))
+    .step(ExampleProcessor())
+    .loader(ExampleLoader())
+    .build()
+)
+
+streaming_pipeline = (
+    Pipeline
+    .builder()
+    .source(ExampleStreamingSource(chunk_size=2))
+    .step(ExampleProcessor())
+    .loader(ExampleLoader())
+    .build()   
+)
+
+if __name__ == "__main__":
+    import asyncio
+    asyncio.run(batch_pipeline.run())
+    #   print result
+    #    ['A', 'B']
+    #    ['C', 'D']
+    #    ['E', 'F']
+
+    #asyncio.run(streaming_pipeline.run())
+    #   print result
+    #  ['ABC', 'ABC']
+    #  ['ABC', 'ABC']
+    #  ['ABC', 'ABC']
+    #       ...
+
+

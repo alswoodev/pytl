530cd1ca82f0ba0dd6cda69e17c6e8a475820bb4
alswoodev <alswoo.dev@gmail.com>
2026-08-08 07:34:11 +0000

    feat: add PolymorphicLoader

---
+++ pytl/loader/PolymorphicLoader.py
@@ -0,0 +1,46 @@
+from ..Loader import Loader
+from typing import get_args, Any
+from collections import defaultdict
+import asyncio
+
+class PolymorphicLoader(Loader[Any]):
+    def __init__(self, *loaders: Loader):
+        self._loaders_by_type: dict[type, list[Loader]] = defaultdict(list)
+
+        for loader in loaders:
+            input_type = self._resolve_input_type(loader)
+            self._loaders_by_type[input_type].append(loader)
+
+    @staticmethod
+    def _resolve_input_type(loader: Loader) -> type:
+        orig_class = getattr(loader, "__orig_class__", None)
+
+        if orig_class is not None:
+            args = get_args(orig_class)
+
+            if args:
+                return args[0]
+
+        raise TypeError(
+            f"{loader.__class__.__name__} must be instantiated as Loader[T]"
+        )
+    
+    async def load(self, chunk: list[Any]) -> None:
+
+        # type -> items
+        buckets: dict[type, list[Any]] = defaultdict(list)
+
+        for item in chunk:
+            buckets[type(item)].append(item)
+        tasks = []
+
+        for item_type, items in buckets.items():
+            loaders = self._loaders_by_type.get(item_type)
+            if not loaders:
+                continue
+
+            for loader in loaders:
+                tasks.append(loader.load(items))
+
+        if tasks:
+            await asyncio.gather(*tasks)
